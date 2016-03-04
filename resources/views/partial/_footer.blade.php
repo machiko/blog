9f17acb908a5d83228ecc1be5f7acf628e97233e
reyes <machikoreo@gmail.com>
2016-03-04 15:25:35 +0800

    initial

---
+++ resources/views/partial/_footer.blade.php
@@ -0,0 +1,16 @@
+<!-- Page Content -->
+<div class="container">
+
+    <!-- Footer -->
+    <footer>
+        <div class="row">
+            <div class="col-lg-12">
+                <p>Copyright &copy; Your Website 2014</p>
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
+    </footer>
+
+</div>
+<!-- /.container -->
